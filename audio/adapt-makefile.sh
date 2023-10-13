ddf2ede16afa88a101df9ae6eae21bee73aa20a8
Yumi Yukimura <me.cafebabe@gmail.com>
2023-10-14 00:02:07 +0800

    sdm660-common: audio: Import audio_policy_configuration.xml to here

--- audio/adapt-makefile.sh
+++ audio/adapt-makefile.sh
@@ -3,3 +3,4 @@ sed -i 's|vendor/qcom/opensource/audio-hal/primary-hal|hardware/qcom-caf/sdm660/
 sed -i 's|^PRODUCT_PACKAGES|NO_PRODUCT_PACKAGES|g' sdm660.mk
 sed -i '/AUDIO_FEATURE_ENABLED_DYNAMIC_LOG/d' sdm660.mk
 sed -i 's|device/qcom/common/media/audio_policy.conf|$(LOCAL_PATH)/audio/audio_policy.conf|g' sdm660.mk
+sed -i 's|hardware/qcom-caf/sdm660/audio/configs/sdm660/audio_policy_configuration.xml|$(LOCAL_PATH)/audio/audio_policy_configuration.xml|g' sdm660.mk
--- audio/sdm660.mk
+++ audio/sdm660.mk
@@ -125,10 +125,10 @@ PRODUCT_COPY_FILES += \
 ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
 ifneq ($(TARGET_USES_AOSP_FOR_AUDIO), true)
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom-caf/sdm660/audio/configs/sdm660/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml
+    $(TOPDIR)$(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml
 endif
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)hardware/qcom-caf/sdm660/audio/configs/sdm660/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(TOPDIR)$(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
